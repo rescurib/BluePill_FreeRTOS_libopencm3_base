ef130a4d8bfe2cc96eafec296e8ea5d8c71ef4e0
rescurib <r.escurib@gmail.com>
2024-10-14 16:56:29 -0600

    feat: add FreeRTOS to the build.

--- Application/src/main.c
+++ Application/src/main.c
@@ -1,7 +1,20 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+static void led_blink_handler(void* parameters);
 
 int main(void) {
+
+    // -- Estructuras de tareas --
+    TaskHandle_t task_1_handle;
+    BaseType_t   status;
+
+    // -- Inicialización de HAL --
+
+    rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+
     // Enable the clock for GPIOC (where the LED is connected on PC13)
     rcc_periph_clock_enable(RCC_GPIOC);
 
@@ -9,15 +22,24 @@ int main(void) {
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
                   GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
-    while (1) {
-        // Toggle the LED (PC13)
-        gpio_toggle(GPIOC, GPIO13);
+    status = xTaskCreate(led_blink_handler,"Led Blink",16,NULL,2,&task_1_handle);
+    configASSERT(status == pdPASS);
 
-        // Simple delay loop
-        for (int i = 0; i < 800000; i++) {
-            __asm__("nop"); // Do nothing
-        }
+    vTaskStartScheduler();
+
+    while (1) 
+    {
+       // loop
     }
 
     return 0;
+}
+
+static void led_blink_handler(void* parameters)
+{
+    // Toggle the LED (PC13)
+    gpio_toggle(GPIOC, GPIO13);
+
+    // delay
+    vTaskDelay(pdMS_TO_TICKS(1000));
 }
